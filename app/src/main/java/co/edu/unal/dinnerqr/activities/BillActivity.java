417b42bad03c9fcd3313152135bd2a850c5a5cf5
yocuervoc <yocuervoc@unal.edu.co>
2019-12-06 05:31:01 -0500

    bill list

--- app/src/main/java/co/edu/unal/dinnerqr/activities/BillActivity.java
+++ app/src/main/java/co/edu/unal/dinnerqr/activities/BillActivity.java
@@ -7,7 +7,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import co.edu.unal.dinnerqr.R;
+import co.edu.unal.dinnerqr.soport.BillListAdapter;
+import co.edu.unal.dinnerqr.soport.BillSoport;
 
 import static co.edu.unal.dinnerqr.activities.DetalleLista.billName;
 import static co.edu.unal.dinnerqr.activities.DetalleLista.billprice;
@@ -22,9 +26,22 @@ public class BillActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bill);
         precio = (TextView)findViewById(R.id.tvBillPrice);
         precio.setText(billprice.get(0).toString());
+        lvBill = (ListView)findViewById(R.id.listabill);
 
+        BillSoport john = new BillSoport("John","12-20-1998");
+        BillSoport steve = new BillSoport("Steve","08-03-1987");
+        BillSoport stacy = new BillSoport("Stacy","11-15-2000");
+        BillSoport ashley = new BillSoport("Ashley","07-02-1999");
+        BillSoport matt = new BillSoport("Matt","03-29-2001");
 
-        //ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,R.layout.list_item_bill, billName);
-        //lvBill.setAdapter(adaptador);
+        ArrayList<BillSoport> peopleList = new ArrayList<>();
+        peopleList.add(john);
+        peopleList.add(steve);
+        peopleList.add(stacy);
+        peopleList.add(ashley);
+        peopleList.add(matt);
+
+        BillListAdapter adapter = new BillListAdapter(this, R.layout.adapter_view_layout, peopleList);
+        lvBill.setAdapter(adapter);
     }
 }
